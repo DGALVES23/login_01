9a58e64169bff56e780cb7cecdf2743804ecacf1
Diego Alves <99363692+DGALVES23@users.noreply.github.com>
2023-03-24 11:20:44 +0100

    Add files via upload
    Upload do exercício de criação de tela de login

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+function validaCampo() {
+
+      let usuario = document.getElementById("usuario").value;
+
+      const senha = "2363196Dw2363"
+
+      console.log(usuario);
+      
+      if (usuario == ""){
+            console.log("erro");
+            
+      }
+
+}
